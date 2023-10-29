738bf8c08b738f2d96b64b97c792b1285a68a6cb
Atho Simatupang <tsmtpng@gmail.com>
2023-10-29 18:57:01 +0700

    Initial commit

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {
+        test: '1234'
+    },
+    gatters: {},
+    actions: {},
+    mutations: {},
+})
+
+export default store
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Login Form</h1>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped></style>
